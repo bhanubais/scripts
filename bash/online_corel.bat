97c5264fcf23ac65206f90f6d5de305020997c00
Bhanu Pratap Singh <bhanubais@gmail.com>
2019-06-28 11:59:47 +0530

    Include Firewall rules for Incoming

--- bash/online_corel.bat
+++ bash/online_corel.bat
@@ -33,6 +33,16 @@ netsh advfirewall firewall add rule name="NUT Corel CUH" dir=out action=block pr
 netsh advfirewall firewall add rule name="NUT Corel DimChain" dir=out action=block program="%ProgramFiles(x86)%\Corel\CUH\v2\dimchain.exe" profile=any description="%ProgramFiles(x86)%\Corel\CUH\v2\dimchain.exe"
 netsh advfirewall firewall add rule name="NUT Corel DIM" dir=out action=block program="%ProgramFiles(x86)%\Corel\CUH\v2\DIM.exe" profile=any description="%ProgramFiles(x86)%\Corel\CUH\v2\DIM.exe"
 
+netsh advfirewall firewall add rule name="NUT - Corel License" dir=in action=block program="%ProgramFiles% (x86)\Common Files\Protexis\License Service\PsiService_2.exe" profile=any description="%ProgramFiles% (x86)\Common Files\Protexis\License Service\PsiService_2.exe"
+netsh advfirewall firewall add rule name="NUT - CorelDRAW" dir=in action=block program="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\CorelDRW.exe" profile=any description="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\CorelDRW.exe"
+netsh advfirewall firewall add rule name="NUT - CorelConverter" dir=in action=block program="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\CdrConv.exe" profile=any description="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\CdrConv.exe"
+netsh advfirewall firewall add rule name="NUT - DIM" dir=in action=block program="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\DIM.exe" profile=any description="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\DIM.exe"
+netsh advfirewall firewall add rule name="NUT Corel UI Server" dir=in action=block program="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\CrlUISvr.exe" profile=any description="%ProgramFiles%\Corel\CorelDRAW Graphics Suite X8\Programs64\CrlUISvr.exe"
+netsh advfirewall firewall add rule name="NUT Corel CUH" dir=in action=block program="%ProgramFiles(x86)%\Corel\CUH\v2\CUH.exe" profile=any description="%ProgramFiles(x86)%\Corel\CUH\v2\CUH.exe"
+netsh advfirewall firewall add rule name="NUT Corel DimChain" dir=in action=block program="%ProgramFiles(x86)%\Corel\CUH\v2\dimchain.exe" profile=any description="%ProgramFiles(x86)%\Corel\CUH\v2\dimchain.exe"
+netsh advfirewall firewall add rule name="NUT Corel DIM" dir=in action=block program="%ProgramFiles(x86)%\Corel\CUH\v2\DIM.exe" profile=any description="%ProgramFiles(x86)%\Corel\CUH\v2\DIM.exe"
+
+
 
 rem Delete Corel Files
 rem ----------------------------------
